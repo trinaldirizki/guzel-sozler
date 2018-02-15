0ce5659021878caa9afce266d1e56446287f340d
Trinaldi Rizki Permana <trinaldirizki@gmail.com>
2018-02-15 18:56:57 +0300

    Added fragments

---
+++ app/src/main/java/com/fit/guzelsozler/fragment/BaseRecyclerFragment.java
@@ -0,0 +1,36 @@
+package com.fit.guzelsozler.fragment;
+
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by fidel on 15.02.2018.
+ */
+
+public abstract class BaseRecyclerFragment extends BaseFragment {
+
+    RecyclerView recyclerView;
+
+    public BaseRecyclerFragment() {
+        setRecyclerViewData();
+    }
+
+    @Override
+    protected void setViewComponents(View view) {
+        recyclerView = view.findViewById(getRecyclerId());
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(getAdapter());
+        setListViewComponents(view);
+    }
+
+    protected abstract void setListViewComponents(View view);
+
+    protected abstract int getRecyclerId();
+
+    protected abstract RecyclerView.Adapter getAdapter();
+
+    protected abstract void setRecyclerViewData();
+}
---
+++ app/src/main/java/com/fit/guzelsozler/util/FragmentUtil.java
@@ -0,0 +1,23 @@
+package com.fit.guzelsozler.util;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+
+import com.fit.guzelsozler.fragment.BaseFragment;
+
+/**
+ * Created by fidel on 15.02.2018.
+ */
+
+public class FragmentUtil {
+
+    /**
+     * Parametre olarak veirlen fragmenti id si verilen layout içeirisinde açar
+     * */
+
+    public static void open(FragmentManager fragmentManager, int layoutId, BaseFragment fragment){
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(layoutId, fragment, fragment.getFragmentTag());
+        transaction.commit();
+    }
+}
