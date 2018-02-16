8a517df62adf61fa2b725b73894b1bf8718e4a8f
trinaldirizki <trinaldirizki@gmail.com>
2018-02-16 18:33:31 +0300

    Added FavoriteFragment.

---
+++ app/src/main/java/com/fit/guzelsozler/fragment/FavoriteFragment.java
@@ -0,0 +1,24 @@
+package com.fit.guzelsozler.fragment;
+
+import android.view.View;
+
+/**
+ * Created by trinaldi on 2/16/18.
+ */
+
+public class FavoriteFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void setViewComponents(View view) {
+
+    }
+
+    @Override
+    public String getFragmentTag() {
+        return null;
+    }
+}
