692eb77f2b63a9d0a322d1b41f3d3229eb6554ea
trinaldirizki <trinaldirizki@gmail.com>
2018-02-16 18:13:43 +0300

    Added HomeFragment.

---
+++ app/src/main/java/com/fit/guzelsozler/fragment/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.fit.guzelsozler.fragment;
+
+import android.view.View;
+
+import com.fit.guzelsozler.R;
+import com.fit.guzelsozler.fragment.BaseFragment;
+
+/**
+ * Created by trinaldi on 2/16/18.
+ */
+
+public class HomeFragment extends BaseFragment {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_home;
+    }
+
+    @Override
+    protected void setViewComponents(View view) {
+
+    }
+
+    @Override
+    public String getFragmentTag() {
+        return null;
+    }
+}
