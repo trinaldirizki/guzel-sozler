09b0fa6325cd04c9d230f1993995313a7937da0e
trinaldirizki <trinaldirizki@gmail.com>
2018-02-17 11:50:35 +0300

    Added RecyclerFragments.

---
+++ app/src/main/java/com/fit/guzelsozler/fragment/HomeRecyclerFragment.java
@@ -0,0 +1,40 @@
+package com.fit.guzelsozler.fragment;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by trinaldi on 2/17/18.
+ */
+
+public class HomeRecyclerFragment extends BaseRecyclerFragment {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public String getFragmentTag() {
+        return null;
+    }
+
+    @Override
+    protected void setListViewComponents(View view) {
+
+    }
+
+    @Override
+    protected int getRecyclerId() {
+        return 0;
+    }
+
+    @Override
+    protected RecyclerView.Adapter getAdapter() {
+        return null;
+    }
+
+    @Override
+    protected void setRecyclerViewData() {
+
+    }
+}
