70fe5149bfe4eb10277cedeedb99cd00ca994374
trinaldirizki <trinaldirizki@gmail.com>
2018-02-27 17:29:37 +0300

    Fixed ads in application.

--- app/src/main/java/com/fit/guzelsozler/MainActivity.java
+++ app/src/main/java/com/fit/guzelsozler/MainActivity.java
@@ -1,47 +1,25 @@
 package com.fit.guzelsozler;
 
-import android.app.Fragment;
-import android.app.FragmentTransaction;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.activeandroid.ActiveAndroid;
-import com.activeandroid.Configuration;
-import com.activeandroid.Configuration.Builder;
 import com.facebook.stetho.Stetho;
-import com.fit.guzelsozler.fragment.CategoryFragment;
-import com.fit.guzelsozler.fragment.CategoryListFragment;
 import com.fit.guzelsozler.fragment.CategoryRecyclerFragment;
 import com.fit.guzelsozler.fragment.CreditsFragment;
-import com.fit.guzelsozler.fragment.FavoriteFragment;
 import com.fit.guzelsozler.fragment.FavoriteRecyclerFragment;
-import com.fit.guzelsozler.fragment.HomeFragment;
 import com.fit.guzelsozler.fragment.HomeRecyclerFragment;
-import com.fit.guzelsozler.fragment.QuoteRecyclerFragment;
-import com.fit.guzelsozler.model.Quote;
 import com.fit.guzelsozler.util.DataUtil;
-import com.fit.guzelsozler.util.DictionaryUtil;
 import com.fit.guzelsozler.util.FragmentUtil;
-import com.fit.guzelsozler.util.SharedPreferenceUtil;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -57,20 +35,20 @@ public class MainActivity extends AppCompatActivity {
 
         MobileAds.initialize(this, getResources().getString(R.string.admob_app_id));
         mInterstitialAd = new InterstitialAd(this);
-        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.setAdUnitId(getResources().getString(R.string.admob_interstitial_id));
         mInterstitialAd.loadAd(new AdRequest.Builder().build());
 
         mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
-        layout = (LinearLayout) findViewById(R.id.fragment_base);
+        layout = findViewById(R.id.fragment_base);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setSubtitle(getString(R.string.title_home));
         }
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         DataUtil.checkDatabase(this);
         FragmentUtil.open(getFragmentManager(), R.id.fragment_base, new HomeRecyclerFragment());
